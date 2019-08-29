877149be3a308ebe2a5e2b5375fdd746cb9e2777
Oliver-Cristian Plamada <oliver.plamada@gmail.com>
2019-08-29 18:15:48 +0300

    Solved bash/two-fer

---
+++ bash/two-fer/two_fer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+function main() {
+  # if [ -z "$1" ]; then
+  #   echo "One for you, one for me."
+  # else
+  #   echo "One for $1, one for me."
+  # fi
+  echo "One for ${1:-"you"}, one for me."
+}
+
+main "$@"
