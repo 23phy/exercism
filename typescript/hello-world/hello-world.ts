69b9cb13369349604e52a8e66c4f9a7c56b9aeac
Oliver-Cristian Plamada <oliver.plamada@gmail.com>
2019-08-29 18:20:26 +0300

    Update typescript/hello-world

--- typescript/hello-world/hello-world.ts
+++ typescript/hello-world/hello-world.ts
@@ -1,7 +1,5 @@
-class HelloWorld {
-    static hello(name: string = "world"): string {
-        return `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}!`;
-    }
+export default class HelloWorld {
+  static hello(name: string = "world"): string {
+    return `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}!`;
+  }
 }
-
-export default HelloWorld
